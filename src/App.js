dba3a922d23d7e4d32522d6cbd077f75a863d750
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-08-15 14:47:09 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Layout from "./pages/Layout";
 import Home from "./components/Home";
+import Countries from "./components/Countries";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // const RouterWrapper = (props) => {
@@ -13,6 +14,7 @@ const App = () => {
     <BrowserRouter>
       <Routes path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/Countries" element={<Countries />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Countries.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+import Nav from "./Nav";
+
+const Countries = () => {
+  return (
+    <div>
+      <Nav />
+      <p> This is a list of Countries</p>
+      <Footer />
+    </div>
+  );
+};
+
+export default Countries;
